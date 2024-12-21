8d52f0956fbad5c35699472c3184bcf89eb062e3
cihaneray <bozkurteraycihan@gmail.com>
2024-12-22 00:26:13 +0300

    Added database_manager.py and updated main.py.

---
+++ src/database_manager.py
@@ -0,0 +1,52 @@
+import psycopg2
+
+
+class DatabaseConnection:
+    def __init__(self):
+        with open('../credential.txt', 'r') as f:
+            f = f.read()
+            credential: list = f.split('\n')
+
+        self.conn = psycopg2.connect(
+            dbname=credential[0],
+            user=credential[1],
+            password=credential[2],
+            host=credential[3],
+            port=credential[4]
+        )
+        self.cursor = self.conn.cursor()
+
+    def open(self):
+        return self.conn, self.cursor
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+class DatabaseManager:
+    def __init__(self):
+        self.conn, self.cursor = DatabaseConnection().open()
+
+    def get_table_column_name(self, table_name: str) -> list:
+        self.cursor.execute(f"SELECT * FROM {table_name} LIMIT 0;")
+        column_variables: list = [desc[0] for desc in self.cursor.description]
+        return column_variables
+
+    def add_to_table(self, table_name: str, values: list):
+        column_variables = self.get_table_column_name(table_name)
+        query_req1: str = ("%s, " * (len(column_variables) - 1))[:-2]
+        col_names: str = ','.join(column_variables[1:])
+        add_query = f"INSERT INTO `{table_name}` ({col_names}) VALUES {query_req1}"
+        self.cursor.execute(add_query, values)
+        try:
+            self.conn.commit()
+            print("Values added to table")
+        except Exception as e:
+            print(e)
+
+    def get_table_values(self, table_name: str) -> list:
+        get_query: str = f"SELECT * FROM `{table_name}`;"
+        self.cursor.execute(get_query)
+        values: list = self.cursor.fetchall()
+        return values
--- src/main.py
+++ src/main.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Merhaba, Flask ile giriş sayfası!"
+    conn, cursor = connect_db()
+    cursor.execute("SELECT * FROM test1;")
+    x = cursor.fetchall()
+    close_db(conn, cursor)
+    return str(x)
 
 def connect_db():
     with open('../credential.txt', 'r') as f:
@@ -29,8 +33,15 @@ def close_db(conn, cursor):
 def main():
     conn, cursor = connect_db()
     print("Connected to database")
+    #cursor.execute("SELECT * FROM test1 LIMIT 0;")
+    #colname = [desc[0] for desc in cursor.description]
+    #query = f"INSERT INTO test1 ({", ".join(colname[1:])}) VALUES ({("%s, " * (len(colname) - 1))[:-2]});"
+    #print(query)
+    #cursor.execute(query, ("test1".strip(), "11010101", "15"))
+    #conn.commit()
+    cursor.execute("SELECT * FROM test1;")
+
     close_db(conn, cursor)
 
 if __name__ == "__main__":
-    main()
     app.run(debug=True)
