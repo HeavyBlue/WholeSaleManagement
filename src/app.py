b6e40d9afd001a3dcb2e6e9c134b46874b14145e
cihaneray <bozkurteraycihan@gmail.com>
2024-12-22 18:36:20 +0300

    Process update

--- src/app.py
+++ src/app.py
@@ -1,52 +1,91 @@
 from database_manager import DatabaseManager
 
-from flask import Flask, render_template, redirect,url_for, request
+from flask import Flask, render_template, redirect, url_for, request
+
 db_manager = DatabaseManager()
 
-customerID=0
+customerID = 0
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET', 'POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     global customerID
     if request.method == 'POST':
-        customer_id = request.form['customer_id']
+        customer_id = int(request.form['customer_id'])
         x = db_manager.login(customer_id)
         if x:
-            customerID=customer_id
+            customerID = customer_id
             return redirect(url_for("home"))
         else:
-            return render_template("login.html")
+            return render_template("pages/login.html")
     else:
-        if(customerID!=0):
+        if customerID != 0:
             return redirect(url_for("home"))
         else:
-          return render_template("login.html")
-    
+            return render_template("pages/login.html")
+
+
 @app.route('/home')
 def home():
     return render_template("home.html")
 
+
 @app.route("/exit")
-def exit():
+def logout():
     global customerID
-    customerID=0
+    customerID = 0
     return redirect(url_for("index"))
+
 
 @app.route("/customer_debts")
 def customer_debts():
     customers = db_manager.check_customer_debts()
-    return render_template("profitable_item.html",customers=customers)
+    return render_template("", customers=customers)
+
 
 @app.route("/monthly_profit")
-def  monthly_profit():
+def monthly_profit():
     profit = db_manager.monthly_profit()
-    return render_template("profit.html",profit=profit)
+    return render_template("pages/monthly-profit.html", item=profit)
+
+
 @app.route("/most_profitable")
 def profitable_item():
     item = db_manager.most_profitable()
-    return render_template("profitable_item.html",item=item)
+    return render_template("", item=item)
+
+
+@app.route("/create_customer")
+def create_customer():
+    return render_template("pages/create-customer.html")
+
+
+@app.route("/create_supplier")
+def create_supplier():
+    return render_template("pages/create-supplier.html")
+
+
+@app.route("/payment")
+def payment():
+    return render_template("pages/create-customer.html")
+
+
+@app.route("/buy_item")
+def buy_item():
+    return render_template("pages/create-customer.html")
+
+
+@app.route("/sell_item")
+def sell_item():
+    return render_template("pages/create-customer.html")
+
+
+@app.route("/inventory_control")
+def inventory_control():
+    return render_template("pages/inventory.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/database_manager.py
+++ src/database_manager.py
@@ -1,6 +1,7 @@
 import psycopg2
 import datetime
 
+
 class DatabaseConnection:
     def __init__(self):
         with open('../credential.txt', 'r') as f:
@@ -37,7 +38,7 @@ class DatabaseManager:
         column_variables = self.get_table_column_name(table_name)
         query_req1: str = ("%s, " * (len(column_variables) - 1))[:-2]
         col_names: str = ','.join(column_variables[1:])
-        add_query = f"INSERT INTO `{table_name}` ({col_names}) VALUES {query_req1}"
+        add_query = f"INSERT INTO {table_name} ({col_names}) VALUES {query_req1}"
         self.cursor.execute(add_query, values)
         try:
             self.conn.commit()
@@ -46,61 +47,59 @@ class DatabaseManager:
             print(e)
 
     def get_table_values(self, table_name: str) -> list:
-        get_query: str = f"SELECT * FROM `{table_name}`;"
+        get_query: str = f"SELECT * FROM {table_name};"
         self.cursor.execute(get_query)
         values: list = self.cursor.fetchall()
         return values
-    
+
     def most_profitable(self):
         query: str = f"SELECT * FROM calculate_monthly_profit();"
         self.cursor.execute(query)
         values: list = self.cursor.fetchall()
         return values
-    
+
     def check_customer_debts(self):
         query: str = f"SELECT * FROM show_customers_whose_debts_are_past_due();"
         self.cursor.execute(query)
         values: list = self.cursor.fetchall()
         return values
-    
+
     def monthly_profit(self):
         query: str = f"SELECT * FROM calculate_monthly_profit();"
         self.cursor.execute(query)
         values: list = self.cursor.fetchall()
         return values
-    
+
     def check_items(self):
         query: str = f"SELECT * FROM Item;"
         self.cursor.execute(query)
         values: list = self.cursor.fetchall()
         return values
-    
+
     def sell_item(self, item_id: int, quantity: int, customer_id: int):
         self.add_to_table("Orders", [customer_id, item_id, quantity])
-    
-    def buy_item(self,supp_item_id: int, quantity: int):
+
+    def buy_item(self, supp_item_id: int, quantity: int):
         date = datetime.datetime.now().strftime("%Y-%m-%d")
         self.add_to_table("Inbound_Items", [supp_item_id, quantity, date])
-    
-    def payment(self,paid_amount: int, customer_id: int):
+
+    def payment(self, paid_amount: int, customer_id: int):
         query = f"UPDATE Payment SET Paid_Amount = Paid_Amount + {paid_amount}, Pending_Amount = Pending_Amount-{paid_amount} WHERE Customer_ID = {customer_id};"
         self.cursor.execute(query)
-    
+
     def get_pending_amount(self, customer_id: int):
         query = f"SELECT Pending_Amount FROM Payment WHERE Customer_ID = {customer_id};"
         self.cursor.execute(query)
         values: list = self.cursor.fetchall()
         return values
-    
+
     def add_customer(self, name: str, second_name: str, address: str, phone: str, email: str, image: str):
         self.add_to_table("Customer", [name, second_name, address, phone, email, image])
 
-    def login(self,customerid: int):
-        query = f"SELECT Customer_ID FROM Customer WHERE Customer_ID = {customerid};"
+    def login(self, customer_id: int):
+        query = f"SELECT Customer_ID FROM Customer WHERE Customer_ID = {customer_id};"
         self.cursor.execute(query)
-        if(self.cursor.fetchall()):
+        if self.cursor.fetchall():
             return True
         else:
             return False
-
-
